6581d21988688805b6b3afcf52504254bca1b9eb
Venomaman <amankumaranand111@gmail.com>
2024-05-27 21:03:11 +0530

    both checkbox and delete fun. added ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
-import { RiCheckboxBlankFill, RiDeleteBin6Line } from "react-icons/ri";
+import { useEffect, useState } from "react";
+import { RiCheckboxBlankFill, RiCheckboxFill, RiDeleteBin6Line } from "react-icons/ri";
 import { TbPencilPlus } from "react-icons/tb";
 import { v4 as uuid } from "uuid";
-import { IoWarning} from 'react-icons/io5'
 
 const toDoListDefault = [
   {
@@ -19,26 +18,61 @@ const toDoListDefault = [
 
 console.log(toDoListDefault);
 
-
-
 function App() {
-  const [toDoList, setToDoList] = useState(toDoListDefault);
   //track the state of pop up
-  const [popUp, setPopUp] = useState(false)
-  function handlePopUp(){
+  const [popUp, setPopUp] = useState(false);
+  const [toDoList, setToDoList] = useState(toDoListDefault);
+
+  function handlePopUp() {
     setPopUp(true);
   }
 
+  function addTask(newTask) {
+    setToDoList((current) => [
+      ...current,
+      {
+        id: uuid(),
+        complete: false,
+        text: newTask,
+      },
+    ]);
+  }
+
+  function toggleCheckbox(id){
+    setToDoList(current=>{
+      return current.map(item=>{
+        if(item.id === id){
+          return{
+            ...item,
+            complete: !item.complete
+          }
+          
+        }
+       
+        return item;
+      })
+    })
+  }
+ 
+  //delete function using the filter method where it compare with id != id..
+  function deleteTask(id){
+    setToDoList(current=>{
+      return current.filter((item)=>item.id != id);
+    });
+  }
+
+
+
   return (
     <>
-      {popUp && <Modal setPopUp={setPopUp}/>}
+      {popUp && <Modal setPopUp={setPopUp} addTask={addTask} />}
       <div className="p-3 border-2 bg-slate-700 text-slate-50">
         <div className=" p-2 grid grid-cols-2 ">
           <div className="grid grid-cols-1">
             <p className="text-4xl p-2 pl-12">My Task</p>
           </div>
           <p className="grid-cols-1 pt-3 text-3xl flex justify-end pr-12 hover:text-cyan-500">
-            <TbPencilPlus onClick={handlePopUp}/>
+            <TbPencilPlus onClick={handlePopUp} />
           </p>
         </div>
       </div>
@@ -52,8 +86,8 @@ function App() {
               <div className="flex pl-2">
                 <div className="grid grid-cols-1 text-2xl">
                   <div className="flex justify-end">
-                    <p className="pr-4 pt-1.5">
-                      <RiCheckboxBlankFill />
+                    <p className="pr-4 pt-1.5"  onClick={()=>toggleCheckbox(listItem.id)}>
+                      {listItem.complete ? <RiCheckboxFill/> : <RiCheckboxBlankFill/>}
                     </p>
                     <p>{listItem.text}</p>
                   </div>
@@ -61,7 +95,7 @@ function App() {
               </div>
               <div className="flex justify-end p-3">
                 <p className="grid grid-cols-1 text-2xl pr-5">
-                  <RiDeleteBin6Line />
+                  <RiDeleteBin6Line onClick={()=> deleteTask(listItem.id)} />
                 </p>
               </div>
             </div>
@@ -72,24 +106,47 @@ function App() {
   );
 }
 
+function Modal({ setPopUp, addTask }) {
+  const [newTask, setNewTask] = useState("");
 
-function Modal({setPopUp}) {
+  function handleAddTask() {
+    if (newTask.trim() !== "") {
+      addTask(newTask.trim());
+      setNewTask("");
+      setPopUp(false);
+    }
+  }
+
   return (
-    <div className='w-screen h-screen bg-black bg-opacity-30 fixed top-0 right-0 flex justify-center items-center'>
-      <div className='bg-white p-10 rounded-md shadow-md'>
-        <h1 className='font-bold text-center text-2xl my-5'>Add Task</h1>
-         <input type="text" className="border-2 w-max p-16"/>
-        <div className='flex justify-between mt-5'>
-          <button className='outline outline-1 outline-[#101f20] bg-[#101f20] text-white py-2 px-4 hover:bg-transparent hover:text-black'
-          onClick={() =>{setPopUp(false)} }
-          >No, Cancel</button>
-          <button className='outline outline-1 outline-[#101f20] hover:bg-[#101f20] hover:text-white py-2 px-4 bg-transparent text-black'
-          onClick={() => console.log("Please like and subscribe")}
-          >Yes, Add Task</button>
+    <div className="w-screen h-screen bg-black bg-opacity-30 fixed top-0 right-0 flex justify-center items-center">
+      <div className="bg-white p-10 rounded-md shadow-md">
+        <h1 className="font-bold text-center text-2xl my-5">Add Task</h1>
+        <input
+          type="text"
+          className="border-2 w-max p-24"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+
+        <div className="flex justify-between mt-5">
+          <button
+            className="outline outline-1 outline-[#101f20] bg-[#101f20] text-white py-2 px-4 hover:bg-transparent hover:text-black"
+            onClick={() => {
+              setPopUp(false);
+            }}
+          >
+            No, Cancel
+          </button>
+          <button
+            onClick={handleAddTask}
+            className="outline outline-1 outline-[#101f20] hover:bg-[#101f20] hover:text-white py-2 px-4 bg-transparent text-black"
+          >
+            Yes, Add Task
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
